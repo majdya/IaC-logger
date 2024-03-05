6f9df96357d21c80cca0b282984634c37a72b145
majdya <majd.ya.1995@gmail.com>
2024-03-05 09:46:49 +0200

    service init

--- LogsUpload/log_upload_service/log_upload_service_stack.py
+++ LogsUpload/log_upload_service/log_upload_service_stack.py
@@ -13,11 +13,4 @@ class LogUploadServiceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        log_upload_service.LogUploadService(self, "Widgets")
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyWidgetServiceQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        log_upload_service.LogUploadService(self, "LogUpload")
